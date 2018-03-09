93f2b7c0009ea478e853c8b23b1a8b955566d65c
Gento <a551.return@gmail.com>
2018-03-09 14:10:59 +0900

    changer

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ echo "Hello heroku, how are you";
 <head>
 <meta charset="UTF-8">
 <title>Document</title>
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/heroku.css">
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
 </head>
@@ -17,6 +17,7 @@ echo "Hello heroku, how are you";
 
 <a href="https://www.youtube.com/watch?v=ePrE8kTAY2E">ここでいいかな</a>
 <p id="top">ヘルプ</p>
+<img src="img/yt_logo.png">
 
  <script src="js/javascript.js"> </script>
 </body>
--- js/javascript.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-  alert("jQueryファイルの読み込み完了でーす。");
-});
