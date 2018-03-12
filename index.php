fa5abc008babf89bf5efa323e9cd083c31225445
Gento <a551.return@gmail.com>
2018-03-12 16:56:36 +0900

    make it better

--- index.php
+++ index.php
@@ -3,35 +3,24 @@
 
 
 // // DBに接続
-  //require('dbconnect.php');
+require('dbconnect.php');
 
-
-
-$url = parse_url(getenv("mysql://bff163e3ca75f8:c676bd1d@us-cdbr-iron-east-05.cleardb.net/heroku_02dda95d941a585?reconnect=true"));
-
-$server = $url["us-cdbr-iron-east-05.cleardb.net"];
-$username = $url["bff163e3ca75f8"];
-$password = $url["c676bd1d"];
-$db = substr($url["heroku_02dda95d941a585"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-
-
-$conn->query();
-$sql = 'select * from user';
-//$stmt = $dbh->prepare($sql);
-$stmt = $conn->prepare($sql);
-  $stmt->execute();
 
 
 // $url = parse_url(getenv("mysql://bff163e3ca75f8:c676bd1d@us-cdbr-iron-east-05.cleardb.net/heroku_02dda95d941a585?reconnect=true"));
 
-//     $server = $url["us-cdbr-iron-east-05.cleardb.net"];
-//     $username = $url["bff163e3ca75f8"];
-//     $password = $url["c676bd1d"];
-//     $db = substr($url["heroku_02dda95d941a585"], 1);
-//     //$db = $url["heroku_02dda95d941a585"];
+// $server = $url["us-cdbr-iron-east-05.cleardb.net"];
+// $username = $url["bff163e3ca75f8"];
+// $password = $url["c676bd1d"];
+// $db = substr($url["heroku_02dda95d941a585"], 1);
 
+// $conn = new mysqli($server, $username, $password, $db);
+
+$sql = 'select * from user';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+$login_member = $stmt->fetch(PDO::FETCH_ASSOC);
 //     $link = mysqli_connect($server, $username, $password, $db);
 //     $result = mysqli_query($link, "select * from user");
 
@@ -48,7 +37,7 @@ $stmt = $conn->prepare($sql);
 //  $dbh->query('SET NAMES utf8');
 
 
-var_dump();
+var_dump($login_member);
 //     while($user = mysqli_fetch_array($result)) {
 //       echo $user['id'], " : ", $user['name'], "<br>";
 //       var_dump($user['id']);
