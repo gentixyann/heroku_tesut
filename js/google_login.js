bc3764bfb9504806be269978f934f19f00ecaa55
Gento <a551.return@gmail.com>
2018-03-13 17:42:39 +0900

    test index

--- js/google_login.js
+++ js/google_login.js
@@ -78,9 +78,10 @@ function sendDb(userID, userName, userEmail, imageUrl){
                  userName: userName,
                  userEmail: userEmail,
                  userID: userID
-             },
+                 },
              function () {
               console.log('dbsave移動');
+              console.log(userName);
              //window.open('json_map.php', '_self');
              //window.open('dbsave.php', '_self');
              }
