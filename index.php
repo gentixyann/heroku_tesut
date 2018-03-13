e98f584e1899362a16d09a4b32866171057edadb
Gento <a551.return@gmail.com>
2018-03-13 18:18:52 +0900

    test index

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ function trans($word,$lang){
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 
-    <script src="https//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     <!--    Goodleのアカウント使用で必要-->
