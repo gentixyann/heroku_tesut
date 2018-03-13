28f05e40b38a3916ea3e33e3354e0c1db7bfd216
Gento <a551.return@gmail.com>
2018-03-13 14:26:20 +0900

    test index

--- index.php
+++ index.php
@@ -147,7 +147,7 @@ function trans($word,$lang){
 
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 
-    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
     <script type="text/javascript" src="js/google_login.js"></script>
     <script src="js/login.js"></script>
