a75645bee75cd754db598ba9019161628a86aa5d
Gento <a551.return@gmail.com>
2018-04-21 15:46:37 +0900

    make it better

--- index.php
+++ index.php
@@ -66,6 +66,15 @@ function trans($word,$lang){
         margin: 0 auto;
     }
 </style>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117970367-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-117970367-1');
+</script>
 
 </head>
 
