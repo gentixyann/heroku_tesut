63c227e075c541196cf7b22b686ba680b636b491
Gento <a551.return@gmail.com>
2018-04-21 16:14:28 +0900

    make it better

--- index.php
+++ index.php
@@ -33,6 +33,15 @@ function trans($word,$lang){
 
 <html>
 <head>
+
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-NVT76Q6');</script>
+  <!-- End Google Tag Manager -->
+
     <meta charset="UTF-8">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -67,19 +76,24 @@ function trans($word,$lang){
     }
 </style>
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117970367-1"></script>
+<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-117970367-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-117970367-1');
-</script>
+</script> -->
 
 </head>
 
 
 <body>
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NVT76Q6"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
     <!-- Navigation
     ================================================== -->
 <div class="hero-background">
