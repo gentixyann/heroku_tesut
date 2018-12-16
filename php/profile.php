49d411c56bee75cfa8e529941e6aa05556d0f09e
Gento <a551.return@gmail.com>
2018-12-16 23:13:10 +0900

    made a php folder

--- php/profile.php
+++ php/profile.php
@@ -86,9 +86,9 @@ require('../dbconnect.php');
      <?php if (isset($_SESSION["id"])){ ?>
        <a href="logout.php">Logout</a>
        <a class="active" href="profile.php">MyPage</a>
-       <a href="post.php">POST</a>
+       <a href="../post.php">POST</a>
        <?php } ?>
-       <a href="json_map.php">*MAP*</a>
+       <a href="../json_map.php">*MAP*</a>
       <a href="javascript:void(0);" style="font-size:30px;" class="icon" onclick="myFunction()">&#9776;</a>
     </div>
 </header>
